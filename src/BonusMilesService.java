48d63c94cb29057b0b85f1a8de15e5ab867e05f1
SERG <serdgant66@mail.ru>
2022-06-29 15:10:05 +0300

    first add

---
+++ src/BonusMilesService.java
@@ -0,0 +1,12 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int tiket = price;
+        int m = 20;
+        int bonus = tiket / m;
+        return bonus;
+    }
+
+
+}
+
+
